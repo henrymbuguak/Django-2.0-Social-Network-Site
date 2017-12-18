027ca9d695128dbb1c5a8c9528c1aade60fa0a63
henrymbuguak <henrymbuguak@gmail.com>
2017-12-18 08:48:32 +0300

    Changes on the models and detail page

--- photos/models.py
+++ photos/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 class Image(models.Model):
@@ -20,4 +21,8 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
             super(Image, self).save(*args, **kwargs)
+
+
+    def get_absolute_url(self):
+        return reverse('image_detail', args=[self.id, self.slug])
 
--- photos/urls.py
+++ photos/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^create/$', views.image_create, name='create_image'),
+    url(r'^image/detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.image_detail, name='image_detail'),
 ]
